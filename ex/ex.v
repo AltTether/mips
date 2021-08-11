5fa577025ae01bc75d48d1149a05e3c22bd4c30f
AltTether <245134e@gs.kochi-tech.ac.jp>
2021-08-12 01:01:15 +0900

    SingleClockMIPSの呼び出しに対応させるためにInsを入力とした形に変える

--- ex/ex.v
+++ ex/ex.v
@@ -1,63 +1,66 @@
 module EX(
-    input reg [31:0] Rdata1, Rdata2, Ed32,
-    input [5:0] ALU,
-    input [5:0] Op,
-    input reg [25:0] Jadr,
-    input reg [31:0] nextPC,
-    output reg [31:0] newPC,
-    output reg [31:0] Result
+          input reg         CLK, RST,
+          input reg [31:0]  newPC, nextPC, Ins,
+          input reg [31:0]  Rdata1, Rdata2, Ed32,
+          output reg [31:0] Result
 );
-    `include "common_param.vh"
+`include "common_param.vh"
 
-    reg [31:0] lo, hi;
-    reg [64:0] reg64;
-    wire [31:0] MUX2Result;
+   reg [31:0]               lo, hi;
+   reg [64:0]               reg64;
+   wire [31:0]              MUX2Result;
 
-    assign MUX2Result = (Op == 6'd0) ? Rdata2 : Ed32;
+   reg [6:0]                Opcode, Funct;
+   assign Opcode = Ins[31:26];
+   assign Funct = Ins[5:0];
 
-    always @(*) begin
-        case(ALU)
-            // hoge
-            ADD:   Result = Rdata1 + MUX2Result;
-            ADDU:  Result = Rdata1 + MUX2Result;
-            SUB:   Result = Rdata1 - MUX2Result;
-            SUBU:  Result = Rdata1 - MUX2Result;
-            AND:   Result = Rdata1 & MUX2Result;
-            OR:    Result = Rdata1 | MUX2Result;
-            XOR:   Result = Rdata1 ^ MUX2Result;
-            NOR:   Result = ~(Rdata1 | MUX2Result);
-            SLT:   Result = Rdata1 < MUX2Result;
-            SLTU:  Result = Rdata1 < MUX2Result;
+   assign MUX2Result = (Opcode == 6'd0) ? Rdata2 : Ed32;
+   assign opcodeOrFunct = (Opcode == 6'd0) ? Funct : Opcode;
 
-            // Immediate Arithmetic Instrunctions
-            ADDI:  Result = Rdata1 + MUX2Result;
-            ADDIU: Result = Rdata1 + MUX2Result;
-            SLTI:  Result = Rdata1 < MUX2Result;
-            SLTIU: Result = Rdata1 < MUX2Result;
-            ANDI:  Result = Rdata1 & MUX2Result;
-            ORI:   Result = Rdata1 | MUX2Result;
-            XORI:  Result = Rdata1 ^ MUX2Result;
+   always @(*) begin
+      case(opcodeOrFunct)
+        // hoge
+        ADD:   Result = Rdata1 + MUX2Result;
+        ADDU:  Result = Rdata1 + MUX2Result;
+        SUB:   Result = Rdata1 - MUX2Result;
+        SUBU:  Result = Rdata1 - MUX2Result;
+        AND:   Result = Rdata1 & MUX2Result;
+        OR:    Result = Rdata1 | MUX2Result;
+        XOR:   Result = Rdata1 ^ MUX2Result;
+        NOR:   Result = ~(Rdata1 | MUX2Result);
+        SLT:   Result = Rdata1 < MUX2Result;
+        SLTU:  Result = Rdata1 < MUX2Result;
 
-            // Shift Op
-            SLL:   Result = Rdata1 << MUX2Result;
-            SRL:   Result = Rdata1 >> MUX2Result;
-            // TODO: research how to do on arithmetic shift
-            SRA:   Result = Rdata1 >> MUX2Result;
-            SLLV:  Result = Rdata1 << MUX2Result;
-            SRLV:  Result = Rdata1 >> MUX2Result;
-            SRAV:  Result = Rdata1 >> MUX2Result;
+        // Immediate Arithmetic Instrunctions
+        ADDI:  Result = Rdata1 + MUX2Result;
+        ADDIU: Result = Rdata1 + MUX2Result;
+        SLTI:  Result = Rdata1 < MUX2Result;
+        SLTIU: Result = Rdata1 < MUX2Result;
+        ANDI:  Result = Rdata1 & MUX2Result;
+        ORI:   Result = Rdata1 | MUX2Result;
+        XORI:  Result = Rdata1 ^ MUX2Result;
 
-            // Mult & Div
-            MFHI:  hi = MUX2Result;
-            MTHI:  hi = MUX2Result;
-            MFLO:  lo = MUX2Result;
-            MTLO:  lo = MUX2Result;
-            MULT:  begin reg64 = Rdata1 * MUX2Result; hi = reg64[63:32]; lo = reg64[31:0]; end
-            MULTU: begin reg64 = Rdata1 * MUX2Result; hi = reg64[63:32]; lo = reg64[31:0]; end
-            DIV:   begin reg64 = Rdata1 / MUX2Result; hi = reg64[63:32]; lo = reg64[31:0]; end
-            DIVU:  begin reg64 = Rdata1 / MUX2Result; hi = reg64[63:32]; lo = reg64[31:0]; end
+        // Shift Op
+        SLL:   Result = Rdata1 << MUX2Result;
+        SRL:   Result = Rdata1 >> MUX2Result;
+        // TODO: research how to do on arithmetic shift
+        SRA:   Result = Rdata1 >> MUX2Result;
+        SLLV:  Result = Rdata1 << MUX2Result;
+        SRLV:  Result = Rdata1 >> MUX2Result;
+        SRAV:  Result = Rdata1 >> MUX2Result;
 
-        endcase
-    end
-  assign newPC = nextPC + 4;
+        // Mult & Div
+        MFHI:  hi = MUX2Result;
+        MTHI:  hi = MUX2Result;
+        MFLO:  lo = MUX2Result;
+        MTLO:  lo = MUX2Result;
+        MULT:  begin reg64 = Rdata1 * MUX2Result; hi = reg64[63:32]; lo = reg64[31:0]; end
+        MULTU: begin reg64 = Rdata1 * MUX2Result; hi = reg64[63:32]; lo = reg64[31:0]; end
+        DIV:   begin reg64 = Rdata1 / MUX2Result; hi = reg64[63:32]; lo = reg64[31:0]; end
+        DIVU:  begin reg64 = Rdata1 / MUX2Result; hi = reg64[63:32]; lo = reg64[31:0]; end
+
+      endcase
+   end // always @ (*)
+
+   assign newPC = nextPC + 4;
 endmodule
--- ex/ex_sim.v
+++ ex/ex_sim.v
@@ -6,149 +6,158 @@ parameter PERIOD=10000;  // (clock period)/2
 
 // constants
 // general purpose registers
-  reg [31:0] rdata1, rdata2, ed32;
-  reg [25:0] jadr;
-  reg [31:0] nextpc;
-  reg [5:0] alu, op;
+   reg clk, rst;
+   reg [31:0] rdata1, rdata2, ed32;
+   reg [31:0] nextpc, ins;
 
 // wires
-  wire [31:0] newpc;
-  wire [31:0] result;
+   wire [31:0] newpc;
+   wire [31:0] result;
 
 // assign statements (if any)
 EX ex1 (
 // port map - connection between master ports and signals/registers
-  .Rdata1(rdata1),
-  .Rdata2(rdata2),
-  .ALU(alu),
-  .Op(op),
-  .Ed32(ed32),
-  .Jadr(jadr),
-  .nextPC(nextpc),
-  .newPC(newpc),
-  .Result(result)
+        .CLK(clk),
+        .RST(rst),
+        .nextPC(nextpc),
+        .newPC(newpc),
+        .Ins(ins),
+        .Rdata1(rdata1),
+        .Rdata2(rdata2),
+        .Ed32(ed32),
+        .Result(result)
 );
 
 initial
-begin
-    alu=#(PERIOD) 6'd32; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
-    ed32=1'b0; jadr=26'd0; nextpc=1'b0;
+  begin
+     // ADD
+     clk=#(PERIOD) 1'd1; rst=1'd0;
+     nextpc=32'd0;
+     ins={6'h00, 20'd0, 6'h20};
+     rdata1=32'd1; rdata2=32'd1; ed32=32'd0;
 
-    alu=#(PERIOD) 6'd33; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // ADDU
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h21};
+     rdata1=32'd1; rdata2=32'd1; ed32=32'd0;
 
-    alu=#(PERIOD) 6'd34; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // SUB
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h22};
+     rdata1=32'd1; rdata2=32'd1; ed32=32'd0;
 
-    alu=#(PERIOD) 6'd35; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // SUBU
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h23};
+     rdata1=32'd1; rdata2=32'd1; ed32=32'd0;
 
-    alu=#(PERIOD) 6'd36; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // AND
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h24};
+     rdata1=32'd1; rdata2=32'd1; ed32=32'd0;
 
-    alu=#(PERIOD) 6'd37; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // OR
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h25};
+     rdata1=32'd1; rdata2=32'd1; ed32=32'd0;
 
-    alu=#(PERIOD) 6'd38; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // XOR
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h26};
+     rdata1=32'd1; rdata2=32'd1; ed32=32'd0;
 
-    alu=#(PERIOD) 6'd39; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // NOR
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h27};
+     rdata1=32'd1; rdata2=32'd1; ed32=32'd0;
 
-    // SLT
-    alu=#(PERIOD) 6'd42; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
-    alu=#(PERIOD) 6'd42; op=6'h00;
-    rdata1=32'd1; rdata2=32'd4;
+     // SLT
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h2A};
+     rdata1=32'd1; rdata2=32'd1; ed32=32'd0;
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h2A};
+     rdata1=32'd1; rdata2=32'd4; ed32=32'd0;
 
-    alu=#(PERIOD) 6'd43; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // SLTU
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h2B};
+     rdata1=32'd1; rdata2=32'd1; ed32=32'd0;
 
-    // ADDI
-    alu=#(PERIOD) 6'd8; op=6'h08;
-    rdata1=32'd1; ed32=32'd1;
+     // ADDI
+     ins=#(PERIOD) {6'h08, 20'd0, 6'h00};
+     rdata1=32'd1; ed32=32'd1;
 
-    // ADDIU
-    alu=#(PERIOD) 6'd9; op=6'h09;
-    rdata1=32'd1; ed32=32'd1;
+     // ADDIU
+     ins=#(PERIOD) {6'h09, 20'd0, 6'h00};
+     rdata1=32'd1; ed32=32'd1;
 
-    // SLTI
-    alu=#(PERIOD) 6'd10; op=6'h0a;
-    rdata1=32'd1; ed32=32'd1;
-    alu=#(PERIOD) 6'd10; op=6'h0a;
-    rdata1=32'd1; ed32=32'd4;
+     // SLTI
+     ins=#(PERIOD) {6'h0A, 20'd0, 6'h00};
+     rdata1=32'd1; ed32=32'd1;
+     ins=#(PERIOD) {6'h0A, 20'd0, 6'h00};
+     rdata1=32'd1; ed32=32'd4;
 
-    // SLTIU
-    alu=#(PERIOD) 6'd11; op=6'h0b;
-    rdata1=32'd1; ed32=32'd1;
+     // SLTIU
+     ins=#(PERIOD) {6'h0B, 20'd0, 6'h00};
+     rdata1=32'd1; ed32=32'd1;
 
-    // ANDI
-    alu=#(PERIOD) 6'd12; op=6'h0c;
-    rdata1=32'd1; ed32=32'd1;
+     // ANDI
+     ins=#(PERIOD) {6'h0C, 20'd0, 6'h00};
+     rdata1=32'd1; ed32=32'd1;
 
-    // ORI
-    alu=#(PERIOD) 6'd13; op=6'h0d;
-    rdata1=32'd1; ed32=32'd1;
+     // ORI
+     ins=#(PERIOD) {6'h0D, 20'd0, 6'h00};
+     rdata1=32'd1; ed32=32'd1;
 
-    // XORI
-    alu=#(PERIOD) 6'd14; op=6'h0e;
-    rdata1=32'd1; ed32=32'd1;
+     // XORI
+     ins=#(PERIOD) {6'h0E, 20'd0, 6'h00};
+     rdata1=32'd1; ed32=32'd1;
 
-    // SLL
-    alu=#(PERIOD) 6'd0; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // SLL
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h00};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // SRL
-    alu=#(PERIOD) 6'd2; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // SRL
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h02};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // SRA
-    alu=#(PERIOD) 6'd3; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // SRA
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h03};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // SLLV
-    alu=#(PERIOD) 6'd4; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // SLLV
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h04};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // SRLV
-    alu=#(PERIOD) 6'd6; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // SRLV
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h06};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // SRAV
-    alu=#(PERIOD) 6'd7; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // SRAV
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h07};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // MFHI
-    alu=#(PERIOD) 6'd16; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // MFHI
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h10};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // MTHI
-    alu=#(PERIOD) 6'd17; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // MTHI
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h11};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // MFLO
-    alu=#(PERIOD) 6'd18; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // MFLO
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h12};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // MTLO
-    alu=#(PERIOD) 6'd19; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // MTLO
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h13};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // MULT
-    alu=#(PERIOD) 6'd24; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // MULT
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h18};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // MULTU
-    alu=#(PERIOD) 6'd25; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // MULTU
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h19};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // DIV
-    alu=#(PERIOD) 6'd26; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // DIV
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h1A};
+     rdata1=32'd1; rdata2=32'd1;
 
-    // DIVU
-    alu=#(PERIOD) 6'd27; op=6'h00;
-    rdata1=32'd1; rdata2=32'd1;
+     // DIVU
+     ins=#(PERIOD) {6'h00, 20'd0, 6'h1B};
+     rdata1=32'd1; rdata2=32'd1;
 end
 endmodule
